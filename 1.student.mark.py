2a39b527672ea16ceb8c79f8265d1184e84d6d12
haianh1502 <laihaianh2002@gmail.com>
2023-03-01 11:01:56 +0700

    practice 1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -3,17 +3,17 @@ courses = []
 marks = {}
 
 #Take information of students
-def student_info(n):
+def input_student(n):
     for student in range(n):
         students.append({"ID of student:":input("ID student: "),"Name of student:":input("Name student: "),"Birthday:":input("Birthday: ")})
 
 #Take information of courses
-def courses_info(c):
+def input_courses(c):
     for course in range(c):
         courses.append({int(input("ID of course: ")):input("Name of course: ")})
 
 #Take marks follow course and student
-def marks_info():
+def input_marks():
     for course in courses:
         for i in course:
             print(i,"-",course[i])
@@ -22,37 +22,49 @@ def marks_info():
                 print(student["ID of student:"])
                 marks[course[i]][student["Name of student:"]] = float(input("Mark: "))
 
-#Print list students, courses or marks of a course
+#List students information
+def list_students():
+    print("\nInformation of student:")
+    for student in students:
+        print("\n")
+        for i in student:
+            print(i+student[i])
+    print("------------")
+
+#List courses information
+def list_courses():
+    print("Information of course:")
+    for course in courses:
+        for i in course:
+            print("\n","ID:",i,"\n","Name course:",course[i])
+    print("------------")
+
+#List marks of student in a course
+def list_marks():
+    x = input("Enter course you want to know mark: ")
+    for nameC, mark in marks.items():
+        if (nameC == x):
+            print(nameC)
+            for key in mark:
+                print("   -" + key + ":",mark[key])
+            break
+        else:
+            print("Don't have this course.")
+            break
+    print("------------")
+
+#Create choices
 def print_info():
     print("1. List students information: \n2. List courses: \n3. List marks of course:")
     choice = int(input("Choose number from 1 to 3: "))
     if (choice == 1):
-        print("\nInformation of student:")
-        for student in students:
-            print("\n")
-            for i in student:
-                print(i+student[i])
-        print("------------")
+        list_students()
         print_info()
     elif (choice == 2):
-        print("Information of course:")
-        for course in courses:
-            for i in course:
-                print("\n","ID:",i,"\n","Name course:",course[i])
-        print("------------")
+        list_courses()
         print_info()
     elif (choice == 3):
-        x = input("Enter course you want to know mark: ")
-        for nameC, mark in marks.items():
-            if (nameC == x):
-                print(nameC)
-                for key in mark:
-                    print("   -" + key + ":",mark[key])
-                break
-            else:
-                print("Don't have this course.")
-                break
-        print("------------")
+        list_marks()
         print_info()
     else:
         print("Wrong input, choose number from 1 to 3.")
@@ -60,14 +72,14 @@ def print_info():
     
 #Main code
 n = int(input("Number of students in class: "))
-student_info(n)
+input_student(n)
 print("------------\n")
 
 c = int(input("The number of courses: "))
-courses_info(c)
+input_courses(c)
 print("------------\n")
 
-marks_info()
+input_marks()
 print("------------\n")
 
 print_info()
